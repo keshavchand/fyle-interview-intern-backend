f8ffb82f97eb8c35627d24f34f985da016310f04
Keshav Chand <chandkeshav001@gmail.com>
2023-04-05 13:16:04 +0530

    Added Test for user not being present

--- tests/user_test.py
+++ tests/user_test.py
@@ -7,3 +7,11 @@ def test_get_user_from_id(user):
     res = user.get_by_id(1)
     assert res.username == 'student1'
     assert res.email == 'student1@fylebe.com'
+
+def test_get_user_not_present_id(user):
+    res = user.get_by_id(1000)
+    assert res == None
+
+def test_get_user_not_present_email(user):
+    res = user.get_by_email('notPresent@flyebe.com')
+    assert res == None
